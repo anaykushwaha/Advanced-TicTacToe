129d410b6270d65a912c16c8d5dcd08b0aa5181a
Anay Kushwaha <anaykushwaha@gmail.com>
2026-07-23 14:17:26 +0530

    UI directory initialized

---
+++ ui/__init__.py
@@ -0,0 +1,10 @@
+""" 
+Advanced Tic Tac Toe 
+User Interface Package 
+
+This package contains every graphical component of the game, 
+including the main window, menus, game board, themes, 
+animations, and dialog boxes 
+
+Author: Anay Kushwaha 
+""" 
